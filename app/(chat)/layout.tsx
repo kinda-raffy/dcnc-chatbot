1c0ab8e7d7306c6974f627ee9f010df4ff46ddc3
Raf <81792786+kinda-raffy@users.noreply.github.com>
2025-06-13 03:39:19 +1000

    feat: default sidebar open

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -22,7 +22,7 @@ export default async function Layout({
         src="https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js"
         strategy="beforeInteractive"
       />
-      <SidebarProvider defaultOpen={!isCollapsed}>
+      <SidebarProvider defaultOpen={true}>
         <AppSidebar user={session?.user} />
         <CommandMenu />
         <SidebarInset>{children}</SidebarInset>
